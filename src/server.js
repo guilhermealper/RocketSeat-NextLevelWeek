bc09d27dff2acf86decd0dce7fb1fb98893674a2
Guilherme Almeida <guilherme.almeida.eng@gmail.com>
2020-06-08 10:18:51 -0300

    template engine, create point responsive dia 4 75%

--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
 const express = require("express")
 const server = express()
 
+//configurando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views",{
+    express: server,
+    noCache: true
+})
 //configurando pasta public
 server.use(express.static("public"))
 
@@ -8,12 +14,17 @@ server.use(express.static("public"))
 // configurar caminhos
 //página inicial
 server.get("/", (req,res) => {
-    res.sendFile(__dirname + "/views/index.html")
+    return res.render("index.html")
 })
 
 server.get("/create-point", (req,res) => {
-    res.sendFile(__dirname + "/views/create-point.html")
+    return res.render("create-point.html")
 })
+
+server.get("/search", (req,res) => {
+    return res.render("search-results.html")
+})
+
 
 //ligar servidor
 server.listen(3000)
